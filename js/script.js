fbd7dd00b6c38c17f3b1491019065875bf70a6e2
Jaycode01 <josephlamidi01@gmail.com>
2024-11-30 12:20:28 +0100

    update - down side of about section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 AOS.init({
-    offset: 100,
+    offset: 80,
     delay: 200,
-    duration: 1000,
-    easing: 'ease-in-out',
+    duration: 500,
+    easing: 'ease-in',
     once: false,
-    mirror: false
+    mirror: false,
+    overflow: 'hidden'
+});
+
+const email = "josephlamidijoslam@gmail.com";
+
+const copyButton = document.getElementById("copyEmailButton");
+const confirmationMessage = document.getElementById("confirmationMessage");
+
+copyButton.addEventListener("click", () => {
+    const tempInput = document.createElement("input");
+    tempInput.value = email;
+    document.body.appendChild(tempInput);
+
+    tempInput.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(tempInput);
+
+    confirmationMessage.style.display = "block";
+
+    setTimeout(() => {
+        confirmationMessage.style.display = "none";
+    }, 2000);
 });
