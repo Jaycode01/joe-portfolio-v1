c1921a66b9463057d715551a4f9a885424cecec3
Jaycode01 <josephlamidi01@gmail.com>
2025-09-19 23:55:10 +0100

    Add job title to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
       <div>Nexon</div>
+      <p>Software Engineer</p>
     </>
   );
 }
