068991e67467dc0104b111f2540514440ada8398
Jaycode01 <josephlamidi01@gmail.com>
2025-09-22 20:24:23 +0100

    I setup experiences data into a json file  and also used it into the UI - I used mapping and importing

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Intro from "./Intro";
+import ExperiencesData from "../data/experiences.json";
+import { Dot } from "lucide-react";
 
 const Main = () => {
   return (
     <div className="main">
       <Intro />
+      <section className="experiences">
+        {ExperiencesData.experiences.map((experience, index) => (
+          <div className="experience" key={index}>
+            <p className="period">{experience.period}</p>
+            <div className="experience-details">
+              <div className="role-and-org">
+                <p className="role">{experience.role}</p>
+                <Dot />
+                <p className="organization">{experience.company}</p>
+              </div>
+              <p className="description">{experience.description}</p>
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
