e77915940342a85e76027149229312541a48445e
Jaycode01 <josephlamidi01@gmail.com>
2025-09-22 20:58:06 +0100

    I ut te experiences code in aseparate component file and also apply CSS to make it stand well.

--- src/app/components/Experiences.tsx
+++ src/app/components/Experiences.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import ExperiencesData from "../data/experiences.json";
+import { Calendar, Dot } from "lucide-react";
 
 const Experiences = () => {
-  return <div></div>;
+  return (
+    <section className="experiences">
+      {ExperiencesData.experiences.map((experience, index) => (
+        <div className="experience" key={index}>
+          <p className="period">
+            <Calendar className="calendar-icon" />
+            <span>{experience.period}</span>
+          </p>
+          <div className="experience-details">
+            <div className="role-and-org">
+              <p className="role">{experience.role}</p>
+              <Dot />
+              <p className="organization">{experience.company}</p>
+            </div>
+            <p className="description">{experience.description}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Experiences;
--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import Intro from "./Intro";
-import ExperiencesData from "../data/experiences.json";
-import { Dot } from "lucide-react";
+import Experiences from "./Experiences";
 
 const Main = () => {
   return (
     <div className="main">
       <Intro />
-      <section className="experiences">
-        {ExperiencesData.experiences.map((experience, index) => (
-          <div className="experience" key={index}>
-            <p className="period">{experience.period}</p>
-            <div className="experience-details">
-              <div className="role-and-org">
-                <p className="role">{experience.role}</p>
-                <Dot />
-                <p className="organization">{experience.company}</p>
-              </div>
-              <p className="description">{experience.description}</p>
-            </div>
-          </div>
-        ))}
-      </section>
+      <Experiences />
     </div>
   );
 };
